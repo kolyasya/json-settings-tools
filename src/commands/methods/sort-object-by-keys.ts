3df573c74c64517d3fced5f7f59c51c4f3bcc6f2
Alexey <fania@milkandcartoons.com>
2021-01-12 18:27:25 +0300

    Update json sorting

--- src/commands/methods/sort-object-by-keys.ts
+++ src/commands/methods/sort-object-by-keys.ts
@@ -1,12 +1,19 @@
-const sortObjectByKeys = <T>(object: T, exceptKeys: string[] | null): T => {
-  let objectKeys = Object.keys(object);
+const sortObjectByKeys = <T>(object: T, exceptKeys: Array<string> | null): T => {
+  const objectKeys = Object.keys(object);
+  let exceptedKeys: Array<string> = [];
   if (exceptKeys) {
-    objectKeys = objectKeys.filter(key => !exceptKeys.includes(key));
+    exceptedKeys = objectKeys.filter(key => exceptKeys.some(except => new RegExp(`^${except}.*`, 'gim').test(key)));
   }
-  
-  const sortedKeys = objectKeys.sort();
 
-  return (exceptKeys || []).concat(sortedKeys).reduce((acc, key) => ({ ...acc, [key]: object[key as keyof T] }), {}) as T
+  const sortedKeys = objectKeys.sort(
+    (a: any, b: any) => a.toLowerCase() < b.toLowerCase()
+      ? -1
+      : a.toLowerCase() > b.toLowerCase()
+        ? 1
+        : 0
+  ).filter(key => !exceptedKeys.includes(key));
+  
+  return exceptedKeys.concat(sortedKeys).reduce((acc, key) => ({ ...acc, [key]: object[key as keyof T] }), {}) as T
 }
 
 export default sortObjectByKeys;
